f0c73b62c637b5540bea2cd1b6f8b8255296ef24
chou victor <chouvictor@choumatoMacBook-Air.local>
2012-03-09 21:41:28 +0800

    Initial Commit

---
+++ srcReaderTests/srcReaderTests.h
@@ -0,0 +1,13 @@
+//
+//  srcReaderTests.h
+//  srcReaderTests
+//
+//  Created by victor chou on 12-3-9.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface srcReaderTests : SenTestCase
+
+@end
---
+++ srcReaderTests/srcReaderTests.m
@@ -0,0 +1,32 @@
+//
+//  srcReaderTests.m
+//  srcReaderTests
+//
+//  Created by victor chou on 12-3-9.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "srcReaderTests.h"
+
+@implementation srcReaderTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in srcReaderTests");
+}
+
+@end
